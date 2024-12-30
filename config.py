28f4037c5f32e03d076c496e8509725dc68331aa
nxivm-bots <imvoid1006@gmail.com>
2024-12-31 00:21:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from logging.handlers import RotatingFileHandler
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7240324742:AAGARczMApTFlTJWdoWoIPyEFTt80yH8Yrs")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7240324742:AAGVhkjzKp51G0J6Y8E_ZU8lgR6DkfJd0wE")
 API_ID = int(os.environ.get("API_ID", "29872536"))
 API_HASH = os.environ.get("API_HASH", "65e1f714a47c0879734553dc460e98d6")
 
